faaadfa99abd3df3e568e62431a35414c84eb681
Eric Dietrich <eric.dietrich@gmail.com>
2018-05-15 14:51:39 -0600

    Fix server db access bug

--- server/app.js
+++ server/app.js
@@ -4,10 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config()
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var cors = require('cors');
+// environment var handling
 
 var app = express();
 
